ca380b664ee1ea0777abf240a02deb5d7e8eba5d
valcomp <valerie.aleida.lopez@gmail.com>
2022-07-31 18:17:11 -0500

    finished player constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -31,13 +31,13 @@ test('gets inventory from player or returns false', () => {
 
     player.inventory = [];
     expect(player.getInventory()).toEqual(false);
-})
+});
 
 test("get player's health value", () => {
     const player = new Player('Dave');
 
     expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
-})
+});
 
 test('check to see if player is alive', () => {
     const player = new Player('Dave');
@@ -46,7 +46,7 @@ test('check to see if player is alive', () => {
     player.health = 0;
     expect(player.isAlive()).toBeFalsy();
     
-})
+});
 
 test('subtracts from player health', () => {
     const player = new Player('Dave');
@@ -57,4 +57,32 @@ test('subtracts from player health', () => {
 
     player.reduceHealth(99999);
     expect(player.health).toBe(0);
+});
+
+test('get attack value of player', () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('add a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const prevCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+    expect(player.inventory.length).toBeGreaterThan(prevCount);
+
 })
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+  
+    player.usePotion(1);
+  
+    expect(player.inventory.length).toBeLessThan(oldCount);
+  });
+
